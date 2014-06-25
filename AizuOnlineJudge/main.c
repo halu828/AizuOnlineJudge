0974db523477023a3f2ee58f625b7540f3c56971
halu828 <t-wakasa@TakumiWakasa-no-MacBook-Pro.local>
2014-06-26 05:08:57 +0900

    Initial Commit

---
+++ AizuOnlineJudge/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  AizuOnlineJudge
+//
+//  Created by TakumiWakasa on 2014/06/26.
+//  Copyright (c) 2014年 TakumiWakasa. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
