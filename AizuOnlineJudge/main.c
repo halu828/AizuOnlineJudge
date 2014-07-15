9aafd60375e47fe42784f17136f403bef3bfbc6f
halu828 <t-wakasa@TakumiWakasa-no-MacBook-Pro.local>
2014-07-15 18:04:36 +0900

    Volume0-0009 Prime Number

--- AizuOnlineJudge/main.c
+++ AizuOnlineJudge/main.c
@@ -6,13 +6,33 @@
 //  Copyright (c) 2014年 TakumiWakasa. All rights reserved.
 //
 
+
 #include <stdio.h>
 
-int main(int argc, const char * argv[])
-{
+int main() {
+	int n, i, j, k;
+	int count;
+	int l[100000];
+	
+	
+	while (scanf("%d", &n) != EOF) {
+		count = 0;
+		if (n >= 2) l[count++] = 2;
+		if (n >= 3) {
+			l[count++] = 3;
+			for (i = 5; i <= n; i+=2) {
+				k = 0;
+				for (j = 1; l[j]*l[j] <= i; j++) {
+					if (i % l[j] == 0) {
+						k = 1;
+						break;
+					}
+				}
+				if (k == 0) l[count++] = i;
+			}
+		}
+		printf("%d\n", count);
+	}
 
-    // insert code here...
-    printf("Hello, World!\n");
     return 0;
 }
-
