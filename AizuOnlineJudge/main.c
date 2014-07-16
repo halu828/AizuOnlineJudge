d5ff9ab27efc8a55b61ee15d626b248fb41f5864
halu828 <t-wakasa@TakumiWakasa-no-MacBook-Pro.local>
2014-07-16 21:54:17 +0900

    0011: Drawing Lots

--- AizuOnlineJudge/main.c
+++ AizuOnlineJudge/main.c
@@ -6,34 +6,25 @@
 //  Copyright (c) 2014年 TakumiWakasa. All rights reserved.
 //
 
-/**
- 三角形の外接円の中心座標を求める計算式
- http://www.h6.dion.ne.jp/~jpe02_tn/ki-5.html
-*/
-
-
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-	int n, i;
-	double x1, x2, x3, y1, y2, y3, a1, b1, c1, a2, b2, c2, px, py, r;
-	
-	scanf("%d", &n);
+	int w, n, i, tmp;
+	int a, b, wary[30];
+
+	scanf("%d",&w);
+	scanf("%d",&n);
+	for (i = 1; i <= w; i++) {
+		wary[i] = i;
+	}
 	for (i = 0; i < n; i++) {
-		scanf("%lf %lf %lf %lf %lf %lf", &x1, &y1, &x2, &y2, &x3, &y3);
-		a1 = 2 * (x2 - x1);
-		b1 = 2 * (y2 - y1);
-		c1 = x1 * x1 - x2 * x2 + y1 * y1 - y2 * y2;
-		a2 = 2 * (x3 - x1);
-		b2 = 2 * (y3 - y1);
-		c2 = x1 * x1 - x3 * x3 + y1 * y1 - y3 * y3;
-		px = (b1 * c2 - b2 * c1) / (a1 * b2 - a2 * b1);
-		py = (c1 * a2 - c2 * a1) / (a1 * b2 - a2 * b1);
-		
-		r = sqrt((px-x1)*(px-x1)+(py-y1)*(py-y1));
-		
-		printf("%.3lf %.3lf %.3lf\n", px, py, r);
+		scanf("%d,%d", &a, &b);
+		tmp = wary[a];
+		wary[a] = wary[b];
+		wary[b] = tmp;
+	}
+	for (i = 1; i <= w; i++) {
+		printf("%d\n", wary[i]);
 	}
 	return 0;
 }
