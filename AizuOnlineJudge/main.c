be24243bafcbdbe37283ce1d4ac28355f8e1ab6c
halu828 <t-wakasa@takumiwasanombp.inf.in.shizuoka.ac.jp>
2014-07-22 22:57:15 +0900

    0015: National Budget

--- AizuOnlineJudge/main.c
+++ AizuOnlineJudge/main.c
@@ -7,24 +7,40 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-	int w, n, i, tmp;
-	int a, b, wary[30];
-
-	scanf("%d",&w);
-	scanf("%d",&n);
-	for (i = 1; i <= w; i++) {
-		wary[i] = i;
-	}
-	for (i = 0; i < n; i++) {
-		scanf("%d,%d", &a, &b);
-		tmp = wary[a];
-		wary[a] = wary[b];
-		wary[b] = tmp;
-	}
-	for (i = 1; i <= w; i++) {
-		printf("%d\n", wary[i]);
+	int n, i, alen, blen, carry, out[82];
+	char a[10000], b[10000];
+	
+	scanf("%d", &n);
+	while (n-- > 0) {
+		scanf("%s", a);
+		scanf("%s", b);
+		alen = (int)strlen(a);
+		blen = (int)strlen(b);
+		if (alen > 80 || blen > 80){
+			printf("overflow\n");
+			continue;
+		}
+		i = 0;
+		carry = 0;
+		while (alen || blen) {
+			out[i] = carry;
+			if (alen) out[i] += a[--alen] - '0';
+			if (blen) out[i] += b[--blen] - '0';
+			carry = out[i] / 10;
+			out[i++] %= 10;
+		}
+		if (carry) out[i] = 1;
+		else i--;
+		if (i >= 80) {
+			printf("overflow\n");
+			continue;
+		}
+		for (; i >= 0; i--)
+			printf("%d", out[i]);
+		printf("\n");
 	}
 	return 0;
 }
